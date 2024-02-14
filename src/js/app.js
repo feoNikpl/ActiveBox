27cc59de763850bbdb7ca854f6572c0fd03cd227
f.nikitin <f.nikitin@defin.by>
2024-02-14 10:21:51 +0300

    fixed testimonials

--- src/js/app.js
+++ src/js/app.js
@@ -6,5 +6,9 @@ new Swiper('.swiper', {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-  
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
 });
